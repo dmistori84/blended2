503b2761d22e681ac9df8ec9766b023a73cc6557
spalnique <lex.sydoruk@gmail.com>
2024-03-24 17:02:41 +0100

    EditForm done

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -1,6 +1,42 @@
-// import { RiSaveLine } from 'react-icons/ri';
-// import { MdOutlineCancel } from 'react-icons/md';
+import { RiSaveLine } from 'react-icons/ri';
+import { MdOutlineCancel } from 'react-icons/md';
+import style from './EditForm.module.css';
+import { useState } from 'react';
 
-export const EditForm = () => {
-  return <h2>EditForm</h2>;
+export const EditForm = ({ defaultValue, onSave, onCancel }) => {
+  const [newValue, setNewValue] = useState(defaultValue.text);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSave({ id: defaultValue.id, text: newValue });
+  };
+
+  const handleChange = (e) => {
+    setNewValue(e.target.value);
+  };
+
+  const handleCancel = () => {
+    onCancel();
+  };
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.submitButton} type="submit">
+        <RiSaveLine color="green" size="16px" />
+      </button>
+
+      <button className={style.editButton} type="button" onClick={handleCancel}>
+        <MdOutlineCancel color="red" size="16px" />
+      </button>
+
+      <input
+        onChange={handleChange}
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="text"
+        required
+        value={newValue}
+        autoFocus
+      />
+    </form>
+  );
 };
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,11 +1,15 @@
 import { GridItem } from '../GridItem/GridItem';
 import style from './TodoListItem.module.css';
 import { Text } from 'components';
-import { RiDeleteBinLine } from 'react-icons/ri';
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const TodoListItem = ({ todo, count, onDelete }) => {
-  const handleClick = () => {
+export const TodoListItem = ({ todo, count, onDelete, onEdit }) => {
+  const handleDelete = () => {
     onDelete(todo.id);
+  };
+
+  const handleEdit = () => {
+    onEdit(todo.id);
   };
 
   return (
@@ -16,11 +20,14 @@ export const TodoListItem = ({ todo, count, onDelete }) => {
         </Text>
         <Text>{todo.text}</Text>
         <button
-          onClick={handleClick}
+          onClick={handleDelete}
           className={style.deleteButton}
           type="button"
         >
           <RiDeleteBinLine size={24} />
+        </button>
+        <button onClick={handleEdit} className={style.editButton} type="button">
+          <RiEdit2Line size={24} />
         </button>
       </div>
     </GridItem>
